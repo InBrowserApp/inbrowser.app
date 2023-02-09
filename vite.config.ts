a96cdb56ef2a88c153b74757ff6a232277c5b3cb
Seedgou <i@zczc.cz>
2023-02-09 08:43:23 +0000

    add icon and vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,42 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), pluginRewriteAll()],
+  plugins: [
+    vue(),
+    pluginRewriteAll(),
+    VitePWA({
+      includeAssets: [
+        "favicon.ico",
+        "favicon-dark.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg",
+      ],
+      manifest: {
+        name: "InBrowser.App",
+        short_name: "InBrowser.App",
+        description:
+          "InBrowser.App is a project to build and collect web apps that fully run in the browser without backend servers.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
