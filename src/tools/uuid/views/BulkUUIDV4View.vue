c41009330af2528316d91b58067d3c4febd60066
Seedgou <i@zczc.cz>
2024-10-09 23:31:33 +0800

    refactor: move uuid description to views

--- src/tools/uuid/views/BulkUUIDV4View.vue
+++ src/tools/uuid/views/BulkUUIDV4View.vue
@@ -6,14 +6,21 @@
     <n-h2 prefix="bar" align-text>Config</n-h2>
     <BulkSizeInput v-model:size="size" />
     <n-h2 prefix="bar" align-text>What is a version 4 UUID?</n-h2>
-    <UUIDV4Description />
+    <n-p>
+      A version 4 UUID is randomly generated. As in other UUIDs, 4 bits are used to indicate version
+      4, and 2 or 3 bits to indicate the variant (102 or 1102 for variants 1 and 2 respectively).
+      Thus, for variant 1 (that is, most UUIDs) a random version-4 UUID will have 6 predetermined
+      variant and version bits, leaving 122 bits for the randomly generated part, for a total of
+      2122, or 5.3×1036 (5.3 undecillion) possible version-4 variant-1 UUIDs. There are half as many
+      possible version-4 variant-2 UUIDs (legacy GUIDs) because there is one less random bit
+      available, 3 bits being consumed for the variant.
+    </n-p>
   </main>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { NH2, NH1 } from 'naive-ui'
-import UUIDV4Description from '../components/uuid/description/UUIDV4Description.vue'
+import { NH2, NH1, NP } from 'naive-ui'
 import BulkUUIDV4Generator from '../components/uuid/generator/BulkUUIDV4Generator.vue'
 import BulkSizeInput from '../components/uuid/config/BulkSizeInput.vue'
 import BackButton from '@/components/navigate/BackButton.vue'
--- src/tools/uuid/views/NilUUIDView.vue
+++ src/tools/uuid/views/NilUUIDView.vue
@@ -4,13 +4,15 @@
     <n-h1 prefix="bar" align-text>Nil UUID</n-h1>
     <NilUUIDGenerator />
     <n-h2 prefix="bar" align-text>What is a Nil UUID</n-h2>
-    <NilUUIDDescription />
+    <n-p>
+      The "nil" UUID, a special case, is the UUID 00000000-0000-0000-0000-000000000000; that is, all
+      bits set to zero.
+    </n-p>
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH2, NH1 } from 'naive-ui'
-import NilUUIDDescription from '../components/uuid/description/NilUUIDDescription.vue'
+import { NH2, NH1, NP } from 'naive-ui'
 import NilUUIDGenerator from '../components/uuid/generator/NilUUIDGenerator.vue'
 import BackButton from '@/components/navigate/BackButton.vue'
 import { useTitle, useDescription } from '@/composables/head'
