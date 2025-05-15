c0e86a41787a32a80ee69da9de28ed6fa51f41fb
Seedgou <i@zczc.cz>
2025-05-15 10:39:43 +0800

    feat(layout): remove vue template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
   </main>
 </template>
