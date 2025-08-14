9a17b3260448eda9f3fb04c3860d1014a23b4489
seedgou <i@rwv.dev>
2025-08-14 11:10:00 +0800

    feat(web): integrate Vite PWA plugin for web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,10 +4,43 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueI18nPlugin({}), vueDevTools()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({}),
+    vueDevTools(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'favicon-dark.ico',
+        'favicon.svg',
+        'apple-touch-icon.png',
+        'masked-icon.svg',
+      ],
+      manifest: {
+        name: 'InBrowser.App',
+        short_name: 'InBrowser.App',
+        description:
+          'InBrowser.App is a project to build and collect web apps that fully run in the browser without backend servers.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
